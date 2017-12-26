34c5f1a1ad3f1bcbff0bd2c04b01575666a8de08
Xander Miller <xander.miller@gmail.com>
2017-12-26 10:51:24 -0500

    adds pycharm cache files to .gitignore

--- tests/transformers/test_vendor_name_normalizer.py
+++ tests/transformers/test_vendor_name_normalizer.py
@@ -2,5 +2,15 @@ import pandas as pd
 import pytest
 from tribble.transformers import vendor_name_normalizer
 
+@pytest.fixture
+def template() -> typing.Dict:
+    return {
+        'contract_period_start': None,
+        'contract_period_end': None,
+        'delivery_date': None,
+        'contract_date': None,
+        'source_fiscal': datetime.date(2017, 1, 1),
+    }
+
 def test_apply() -> None:
     pass
